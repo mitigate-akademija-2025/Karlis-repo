91548aee10e1f401ef225e2c0e1754869e663ed3
Karlis Kazulis <35068277+KarlisKazulis@users.noreply.github.com>
2025-08-06 07:08:52 +0000

    test1 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,48 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
+
+
+class Animal
+    attr_accessor  :name, :legs_count
+    def initialize(name, legs_count)
+        @name = name
+        @legs_count = legs_count
+    end
+
+    def speak
+      puts "roar"
+    end
+end
+
+lion = Animal.new("Lion", 5)
+lion.speak
+
+class Dog
+  def initialize(name, legs_count)
+    @name = name
+    @legs_count = legs_count
+  end
+
+  def speak
+    puts "vau vau"
+  end
+end
+
+
+doggo = Dog.new("Sunis", 6)
+doggo.speak
+
+
+class AnotherCat < Animal
+  def speak
+    super
+    puts "meow"
+  end
+end
+
+bobcat = AnotherCat.new("Bobcat", 4)
+bobcat.speak
+
+
+
